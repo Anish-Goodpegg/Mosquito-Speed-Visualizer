28665010bbd48a48c8644953f621c6e67d4bc2ea
Anish-Goodpegg <anish.goodpegg@gmail.com>
2025-06-24 10:22:52 +0530

    kl - 2025-06-24 10:22:52

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 # 1. Find path if you don't know it - pwd
 # 2. Make sure you have the right permissions - chmod +x pathname/deploy.sh
-# 3. Write ~/.zshrc
+# 3. Write nano ~/.zshrc
 # 4. alias deploy or (any name for git push)='/path/to/deploy.sh'
 # 5. Ctrl + O then Enter to save
 # 6. Ctrl + X to exit
@@ -19,5 +19,5 @@ echo "🚀 Committing and pushing to GitHub..."
 git add .
 git commit -m "$commit_message - $(date +'%Y-%m-%d %H:%M:%S')"
 git push origin main
-
+#hcikd kfcew
 echo "✅ Done!"
